20d8c7078a6009ebc349d1165e2d9d1386b22a84
bandinth <74702934+bandinth@users.noreply.github.com>
2023-12-11 17:06:20 +0200

    add commets to file

--- test_GitHub.py
+++ test_GitHub.py
@@ -1,17 +1,19 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import*
 
+
+#Створення додатку
 app=QApplication([])
 win=QWidget()
-
+#Задаємо розмір екрану
 win.resize(200,200)
 win.setWindowTitle('Тест ГітХаб')
-
+#Створюємо підпис з текстом
 title=QLabel('Тестовий додаток для гітХаб')
-
+#Створення ліній
 line_h=QHBoxLayout()
 line_h.addWidget(title)
-
+#Добавлення ліній в програму
 win.setLayout(line_h)
 
 win.show()
